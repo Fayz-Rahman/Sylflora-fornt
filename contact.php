e3734430ddac2c4af483a39bd7c8e59e2bf20bf1
Fayz-Rahman <xrosdarken@gmail.com>
2025-07-30 07:13:32 +0600

    update file structure, and fix UI bugs

--- contact.php
+++ contact.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us - Sylflora</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Hind+Siliguri:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body class="font-bangla">
 
@@ -51,7 +51,7 @@
 
 
   <?php include 'bottom-nav.php'; ?>
-    <script src="data.js"></script>
-    <script src="global.js"></script>
-    <script src="contact.js"></script> </body>
+    <script src="./js/data.js"></script>
+    <script src="./js/global.js"></script>
+    <script src="./js/contact.js"></script> </body>
 </html>
